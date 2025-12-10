071ecdc24de104010ec01746262ef82ed6cec0eb
mynzi1 <87758193+mynzi1@users.noreply.github.com>
2025-12-10 10:28:43 +0900

    CORS setting

--- src/main/java/web/mvc/config/SecurityConfig.java
+++ src/main/java/web/mvc/config/SecurityConfig.java
@@ -56,8 +56,8 @@ public class SecurityConfig {
                         CorsConfiguration configuration = new CorsConfiguration();
                         //configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
                         //configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:4173"));
-                        configuration.setAllowedOrigins(Arrays.asList("http://43.203.170.229", "http://43.203.170.229:80"));
-                        configuration.setAllowedOrigins(Arrays.asList("http://heejung.n-e.kr", "https://heejung.n-e.kr"));
+                        configuration.setAllowedOrigins(Arrays.asList("http://54.180.240.89", "http://54.180.240.89:80"));
+                        configuration.setAllowedOrigins(Arrays.asList("http://my-kosta-test.kro.kr", "https://my-kosta-test.kro.kr"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
 
